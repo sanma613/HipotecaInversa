0032449751f9b3e4f2113f452160c033422cdc2e
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-04-03 09:37:45 -0500

    Implementacion buttom y label

--- src/view/gui/hipoteca_inversa_gui.py
+++ src/view/gui/hipoteca_inversa_gui.py
@@ -13,8 +13,42 @@ sys.path.append("src")
 # Logica de la tarjeta de credito
 from model.hipoteca_inversa import Hipoteca
 
-class hipoetca_inversa(App):
+class hipotecaApp(App):
     def build(self):
-        cotenedor = GridLayout(cols=2, padding=20, spacing=20)
+        contenedor = GridLayout(cols=2, padding=20, spacing=20)
+
+        contenedor.add_widget(Label(text="Edad del usuario"))
+        self.edad = TextInput(font_size=30)
+        contenedor.add_widget(self.edad)
+
+        contenedor.add_widget(Label(text="Precio de la vivienda"))
+        self.precio_vivienda = TextInput(font_size=30)
+        contenedor.add_widget(self.precio_vivienda)
+
+        contenedor.add_widget(Label(text="Porcentaje del precio real"))
+        self.porcentaje_precio = TextInput(font_size=30)
+        contenedor.add_widget(self.porcentaje_precio)
+        
+        contenedor.add_widget(Label(text="Número de cuotas"))
+        self.cuotas = TextInput(font_size=30)
+        contenedor.add_widget(self.cuotas)
+
+        contenedor.add_widget(Label(text="Tasa de interés mensual"))
+        self.tasa_interes = TextInput(font_size=30)
+        contenedor.add_widget(self.tasa_interes)
+
+        self.resultado = Label()
+        contenedor.add_widget(self.resultado)
+
+        calcular = Button(text="Calcular Hipoteca", font_size=40)
+        contenedor.add_widget(calcular)
+
+        #calcular.bind(on_press=self.calcular_hipoteca)
+        
+        return contenedor
+    
+hipotecaApp().run()
+
+
         
     
