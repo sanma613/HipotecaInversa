9ead350d803cf6928a8f14b0c3885179f4830d62
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-04-08 16:45:31 -0500

    correciones e implementacion GUI

--- src/view/gui/hipoteca_inversa_gui.py
+++ src/view/gui/hipoteca_inversa_gui.py
@@ -37,20 +37,67 @@ class hipotecaApp(App):
         contenedor.add_widget(Label(text="Tasa de interés mensual"))
         self.tasa_interes = TextInput(font_size=30)
         contenedor.add_widget(self.tasa_interes)
-
-        self.resultado = Label()
-        contenedor.add_widget(self.resultado)
-
-        calcular = Button(text="Calcular Hipoteca", font_size=40)
+        
+        # Botón calcular (ocupa dos columnas)
+        calcular = Button(text="Calcular", font_size=40)
+        calcular.bind(on_press=self.calcular_hipoteca)
         contenedor.add_widget(calcular)
+        contenedor.add_widget(Label()) 
+        
+        # Label para ingreso mensual
+        contenedor.add_widget(Label(text="Ingreso mensual:", font_size=20, halign="left"))
+        self.resultado_ingreso = Label(font_size=20, halign="right")
+        self.resultado_ingreso.bind(size=self.resultado_ingreso.setter('text_size'))  
+        contenedor.add_widget(self.resultado_ingreso)
 
-        #calcular.bind(on_press=self.calcular_hipoteca)
+        # Label para deuda total
+        contenedor.add_widget(Label(text="Deuda total:", font_size=20, halign="left"))
+        self.resultado_deuda = Label(font_size=20, halign="right")
+        self.resultado_deuda.bind(size=self.resultado_deuda.setter('text_size'))
+        contenedor.add_widget(self.resultado_deuda)
         
         return contenedor
     
-    def calcular_hipoteca(self, instance):
-        pass
-        
+    def calcular_hipoteca(self, sender):
+        try:
+            """Validacion de los campos"""
+            self.validar_entradas()
+            hipoteca = Hipoteca(
+                edad = int(self.edad.text),
+                precio_de_la_vivienda = float(self.precio_vivienda.text),
+                porcentaje_precio_real = float(self.porcentaje_precio.text),
+                total_cuotas = int(self.cuotas.text),
+                tasa_de_interes_mensual = float(self.tasa_interes.text)
+            )
+            
+            # Calcular ingreso mensual
+            ingreso_mensual = hipoteca.calcular_ingreso_mensual()
+            
+            # Calcular deuda total usando el ingreso mensual anterior
+            deuda_total = hipoteca.calcular_deuda_total(ingreso_mensual)
+            
+             # Mostrar el resultado en el label
+            """Ingreso mensual"""
+            resultado_ingreso_mensual = round(ingreso_mensual)
+             
+            """deuda total"""
+            resultado_deuda_total = round(deuda_total)
+            
+            self.resultado_ingreso.text = str(resultado_ingreso_mensual)
+            
+            self.resultado_deuda.text = str(resultado_deuda_total)
+             
+        except ValueError as err:
+            self.resultado_ingreso.text = ""
+            self.resultado_deuda.text = (
+            "El valor ingresado no es válido. Por favor ingrese números"
+             )
+        except Exception as err:
+            self.mostrar_error(err)
+            
+    def validar_entradas(self):
+       pass
+
 if __name__ == "__main__":
     hipotecaApp().run()
     
