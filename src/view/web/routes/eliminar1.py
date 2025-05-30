32276cdb5111f0d9ea65aefe31ad61c6f41a0be7
Simon07-09 <simoncho0907@gmail.com>
2025-05-29 21:19:38 -0500

    refactorizacion de los metodos de eliminar

--- src/view/web/routes/eliminar1.py
+++ src/view/web/routes/eliminar1.py
@@ -7,24 +7,6 @@ from controller.PropiedadesController import PropiedadesController
 
 eliminar_blueprint = Blueprint('eliminar_blueprint', __name__)
 
-@eliminar_blueprint.route ("/eliminar_cliente", methods=["POST", "GET"])
-def eliminar_cliente():
-    if request.method == "POST":
-        id = request.form["id"]
-        clientesController = ClientesController()
-        clientesController.eliminar(id)
-        return redirect(url_for("index_blueprint.index"))
-    return render_template("eliminar.html")
-
-@eliminar_blueprint.route ("/eliminar_propiedad", methods=["POST", "GET"])
-def eliminar_propiedad():
-    if request.method == "POST":
-        id = request.form["id"]
-        propiedadesController = PropiedadesController()
-        propiedadesController.eliminar(id)
-        return redirect(url_for("index_blueprint.index"))
-    return render_template("eliminar.html")
-
 @eliminar_blueprint.route("/eliminar", methods=["POST", "GET"])
 def eliminar():
     if request.method == "POST":
